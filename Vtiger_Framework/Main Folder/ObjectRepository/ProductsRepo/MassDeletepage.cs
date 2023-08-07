7e612d65f19d66c2d4e22d8eee3e9a5aafe63b8d
Veenashekhar18 <veenaraichur58@gmail.com>
2023-08-07 16:39:58 +0530

    Delete page and Massdelete page is added

---
+++ Vtiger_Framework/Main Folder/ObjectRepository/ProductsRepo/MassDeletepage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vtiger_Framework.Main_Folder.ObjectRepository.ProductsRepo
+{
+    internal class MassDeletepage
+    {
+        [FindsBy(How = How.XPath, Using = "//a[.='Products']")]
+        public IWebElement productslink; //click on Products link
+        [FindsBy(How = How.XPath, Using = "//input[@name='selectall']")]
+        public IWebElement selectallcheckbox; //select the checkbox
+        [FindsBy(How = How.XPath, Using = "//input[@value='Delete' and @fdprocessedid='ymc8cj']")]
+        public IWebElement deletebtn; //click on delete button
+
+        public Massdeletepage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Productslink()
+        {
+            productslink.Click();
+        }
+
+        public void Selectallcheckbox()
+        {
+            selectallcheckbox.Click();
+        }
+
+        public void Massdelete()
+        {
+            deletebtn.Click();
+        }
+    }
+}
